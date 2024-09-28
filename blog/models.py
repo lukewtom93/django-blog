ae224d3d7676e4e2e9a7a93f769c415d5fe7a074
Luke Tomlinson <lukewtom93@gmail.com>
2024-09-28 18:27:21 +0000

    meta data added

--- blog/models.py
+++ blog/models.py
@@ -27,4 +27,9 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ["-created_on"]
+    def __str__(self):
+        return f"comment {self.body} by {self.author}"
+
     
