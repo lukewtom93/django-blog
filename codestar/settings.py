3ff335f1616bfe144398f48202db9c582bbfdf8d
Luke Tomlinson <lukewtom93@gmail.com>
2024-09-29 10:47:39 +0000

    Added generic view for mome page

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w29v996tcuy-&zek=%_q$_701pmx!f8a@t03j%8jhwxns&9k-i'
-#SECRET_KEY = os.environ.get("SECRET_KEY")
+# SECRET_KEY = 'django-insecure-w29v996tcuy-&zek=%_q$_701pmx!f8a@t03j%8jhwxns&9k-i'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
