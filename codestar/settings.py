6c0561f1b1bcacaa2dac385ea431d7c9463ba632
Luke Tomlinson <lukewtom93@gmail.com>
2024-10-06 12:04:37 +0000

    server secret key

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,12 +25,12 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w29v996tcuy-&zek=%_q$_701pmx!f8a@t03j%8jhwxns&9k-i'
-# SECRET_KEY = os.environ.get("SECRET_KEY")
+# SECRET_KEY = 'django-insecure-w29v996tcuy-&zek=%_q$_701pmx!f8a@t03j%8jhwxns&9k-i'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = Fasle
+DEBUG = False
 ALLOWED_HOSTS = [ '8000-lukewtom93-djangoblog-qphz2be7zjs.ws.codeinstitute-ide.net', '.herokuapp.com']
 
 
